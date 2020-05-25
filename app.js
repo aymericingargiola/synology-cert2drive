747f9a3f6a2620c273d81517d3a8848801a2e530
Aymeric Ingargiola <aymericfbm@gmail.com>
2020-05-25 14:25:23 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const path = require("path");
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+const CronJob = require('cron').CronJob;
+const config = require('./config.json');
+const globalFunctions = require('./global-functions');
+const tempFolder = path.normalize(__dirname + "/temp");
+const tempFolderArchive = path.normalize(tempFolder + "/_archive");
+let settings, domains;
+
+async function getDomainsFolder() {
+    let domain; var domainsArray = [];
+    const getDirectories = fs.readdirSync(tempFolderArchive).filter(folder => fs.statSync(path.join(tempFolderArchive, folder)).isDirectory());
+    await globalFunctions.asyncForEach(getDirectories, async (folder) => {
+        try {
+            domain = await readFileAsync(`${tempFolderArchive}\\${folder}\\renew.json`, 'utf8');
+            domain = JSON.parse(domain);
+            domain = domain.domains;
+        }
+        catch (err) {
+            domain = "synology";
+        }
+        domainsArray.push({ domain: domain, folder: folder });
+    });
+    console.log(domainsArray);
+    return domains = domainsArray;
+}
+
+async function updateCert(dest) {
+    console.time(`Certification updated`);
+    const domainFolder = domains.filter(domain => domain.domain === dest.domain)[0].folder;
+    console.log(`Copy certificates from ${dest.domain} (${domainFolder}) to -> ${dest.destination}`);
+    await globalFunctions.execShellCommand(`scp -P ${settings.port} -i ${settings.privateKey} ${settings.username}@${settings.host}:/usr/syno/etc/certificate/_archive/${domainFolder}/* ${dest.destination}`);
+    return console.timeEnd(`Certification updated`);
+}
+
+function updateCertJob() {
+    var thisUpdateCertJob = new CronJob('*/10 * * * * *', async function () {
+        console.log("\n### Start")
+        console.time("\nTask time");
+        if (!fs.existsSync(tempFolder)) {
+            fs.mkdirSync(tempFolder);
+        } else if (fs.readdirSync(tempFolder).length != 0) {
+            console.log("\nClean temp folder");
+            await globalFunctions.cleanFolder(tempFolder);
+        }
+        console.time("\nGet certifications from synology");
+        await globalFunctions.execShellCommand(`scp -P ${settings.port} -i ${settings.privateKey} -r ${settings.username}@${settings.host}:/usr/syno/etc/certificate/_archive/ ${tempFolder}`);
+        console.timeEnd("\nGet certifications from synology");
+        console.group("\nSynology domains found :");
+        await getDomainsFolder();
+        console.groupEnd("Synology domains found :");
+        console.group("\nUpdate certificates :");
+        await globalFunctions.asyncForEach(settings.certDestinations, async (dest, index) => {
+            await globalFunctions.cleanFolder(dest.destination);
+            await updateCert(dest, index);
+        });
+        console.groupEnd("Update certificates :");
+        console.log("\nClean temp folder");
+        await globalFunctions.cleanFolder(tempFolder);
+        console.timeEnd("\nTask time");
+        console.log("\n### End")
+    }, null, true, 'Europe/Paris');
+    thisUpdateCertJob.start();
+}
+
+
+
+async function init() {
+    return settings = {
+        host: config.synologySettings.host != "" ? config.synologySettings.host : await globalFunctions.missingSetting("input", "hostname", "Host name"),
+        port: config.synologySettings.port != "" ? config.synologySettings.port : await globalFunctions.missingSetting("input", "port", "port"),
+        username: config.synologySettings.username != "" ? config.synologySettings.username : await globalFunctions.missingSetting("input", "username", "User name"),
+        privateKey: config.sshSettings.privateKey != "" ? config.sshSettings.privateKey : await globalFunctions.missingSetting("input", "privatekey", "Private Key path"),
+        certFolders: config.synologySettings.certFolders != "" ? config.synologySettings.certFolders : await globalFunctions.missingSetting("input", "certpath", "Certification folder name on Synology"),
+        certDestinations: config.synologySettings.certDestinations != "" ? config.synologySettings.certDestinations.filter(dest => dest.destination.indexOf(".") === 0).length > 0 ? globalFunctions.processPath(config.synologySettings.certDestinations) : config.synologySettings.certDestinations : await globalFunctions.missingSetting("input", "certpath", "Certification destination path"),
+    }
+}
+
+init().then(updateCertJob());
---
+++ global-functions.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require("path");
+const execute = require('child_process');
+const inquirer = require('inquirer');
+const rmdir = require('rimraf');
+const projectPath = __dirname;
+module.exports = {
+    execShellCommand: function (cmd) {
+        return new Promise((resolve, reject) => {
+            execute.exec(cmd, (error, stdout, stderr) => {
+                if (error) {
+                    console.warn(error);
+                }
+                resolve(stdout ? stdout : stderr);
+            });
+        });
+    },
+    asyncForEach: async function (array, callback) {
+        for (let index = 0; index < array.length; index++) {
+            await callback(array[index], index, array);
+        }
+    },
+    cleanFolder: async function (folder) {
+        fs.readdir(folder, (err, files) => {
+            if (err) throw err;
+            for (const file of files) {
+                fs.lstat(folder + "\/" + file, (err, stats) => {
+                    if (err) {
+                        return err; //Handle error
+                    }
+                    if (stats.isDirectory()) {
+                        rmdir(folder + "\/" + file, function (error) { return error });
+                    }
+                    else if (stats.isFile()) {
+                        fs.unlink(path.join(folder, file), err => {
+                            if (err) throw err;
+                        });
+                    }
+                });
+            }
+        });
+    },
+    processPath: function (paths) {
+        var array = [];
+        var correctPath;
+        paths.forEach(function (thisPath) {
+            if (thisPath.destination.indexOf(".") === 0) {
+                correctPath = thisPath.destination.replace('.', projectPath.toString());
+            } else {
+                correctPath = thisPath.destination;
+            }
+            if (!fs.existsSync(correctPath)) {
+                fs.mkdirSync(correctPath);
+            }
+            const destination = {
+                domain: thisPath.domain,
+                destination: path.normalize(correctPath)
+            }
+            array.push(destination);
+        });
+        return array;
+    },
+    missingSetting: async function (type, name, message, defaultValue) {
+        const prompt = await inquirer.prompt([
+            {
+                type: type,
+                name: name,
+                message: message,
+                default: defaultValue ? defaultValue : undefined
+            }
+        ]);
+        return prompt[name];
+    }
+}
